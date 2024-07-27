c7c51260411007516601bdcc147594d5b6684e86
LipeSou <felipesoutomdt@gmail.com>
2024-07-27 14:27:48 -0300

    feat: create error handling

---
+++ src/error-handler.ts
@@ -0,0 +1,25 @@
+import type { FastifyInstance } from "fastify"
+import { ClientError } from "./errors/client-error"
+import { ZodError } from "zod";
+
+type FastifyErrorHandler = FastifyInstance['errorHandler']
+
+
+export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
+
+    if(error instanceof ClientError) {
+        return reply.status(400).send({
+            message: error.message
+        })
+    }
+
+    if (error instanceof ZodError) {
+        return reply.status(422).send({
+            message: "Validation error",
+            issues: error.issues // Detalhes do erro de validação
+        });
+    }
+    
+    return reply.status(500).send({ message: 'Internal server error teste' }) 
+} 
+
