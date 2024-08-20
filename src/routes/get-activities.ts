06123c91f0d4c051df959915b1b5a76c842fd18d
LipeSou <felipesoutomdt@gmail.com>
2024-08-20 11:05:38 -0300

    fix: time zone

--- src/routes/get-activities.ts
+++ src/routes/get-activities.ts
@@ -32,7 +32,7 @@ export async function getActivities(app: FastifyInstance) {
             throw new ClientError("Trip not found")
         }
 
-        const differenceInDaysBetweenTripsStartAndEnd = dayjs(trip.ends_at).diff(trip.starts_at, 'days')
+        const differenceInDaysBetweenTripsStartAndEnd = dayjs(trip.ends_at).tz("America/Sao_Paulo").endOf('day').diff(trip.starts_at, 'days')
 
         const activities = Array.from({ length: differenceInDaysBetweenTripsStartAndEnd + 1 }).map((_, index) => {
             const date = dayjs(trip.starts_at).add(index, 'days')
